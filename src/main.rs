f1197faf8cf2a9a11ab1139be5aed44330d684af
Valera6 <v79166789533@gmail.com>
2024-01-17 14:45:02 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod utils;
 use clap::{Args, Parser, Subcommand};
 use config::Config;
 use utils::ExpandedPath;
+use v_utils::klines::Timeframe;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -17,8 +18,19 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
 	/// Start the program
-	Start(NoArgs),
+	New(PositionArgs),
 }
+#[derive(Args)]
+struct PositionArgs {
+	#[arg(long)]
+	/// percentage of the total balance to use
+	size: f64,
+	#[arg(long)]
+	/// timeframe, in the format of "1m", "1h", "3M", etc.
+	/// determines the target period for which we expect the edge to persist.
+	tf: Timeframe,
+}
+
 #[derive(Args)]
 struct NoArgs {}
 
@@ -34,9 +46,12 @@ async fn main() {
 	};
 
 	match cli.command {
-		Commands::Start(_) => {
+		Commands::New(position_args) => {
 			let _balance = exchange_interactions::compile_total_balance(config.clone()).await;
 			dbg!(&_balance);
+
+			println!("{}", &position_args.tf);
+			dbg!(&position_args.size);
 		}
 	}
 }
