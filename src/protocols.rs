cd814df768d4ae5a12dc5029da49443424678bc2
Valera6 <v79166789533@gmail.com>
2024-01-19 22:05:13 +0000

    fix: moved custom format macro into v_utils

--- src/protocols.rs
+++ src/protocols.rs
@@ -3,9 +3,9 @@ use anyhow::{Error, Result};
 use std::collections::HashMap;
 use std::fmt;
 use std::str::FromStr;
+use v_utils::data::compact_format::COMPACT_FORMAT_DELIMITER;
+use v_utils::init_compact_format;
 use v_utils::trades::Timeframe;
-
-const PROTOCOL_ARGS_DELIMITER: char = ':';
 
 // de impl on this will split upon a delimiter, then have several ways to define the name, which is the first part and translated directly; while the rest is parsed.
 //TODO!: move away from a vec of protocols, and embrace specification of their functions.
@@ -19,7 +19,7 @@ impl FromStr for Protocol {
 	type Err = anyhow::Error;
 
 	fn from_str(s: &str) -> Result<Self> {
-		let mut parts = s.splitn(2, PROTOCOL_ARGS_DELIMITER);
+		let mut parts = s.splitn(2, COMPACT_FORMAT_DELIMITER);
 		let name = parts.next().ok_or_else(|| Error::msg("No protocol name"))?;
 		let params = parts.next().ok_or_else(|| Error::msg("Missing parameter specifications"))?;
 		let protocol: Protocol = match name.to_lowercase().as_str() {
@@ -41,68 +41,11 @@ impl fmt::Display for Protocol {
 	}
 }
 
-// my_format! macro {{{
-macro_rules! my_format {
-($name:ident, [ $(($field:ident, $field_type:ty)),* ]) => {
-#[derive(Clone, Debug)]
-pub struct $name {
-$(
-$field: $field_type,
-)*
-}
-///NB: Note that FromStr takes string withot $name, while Display prints it with $name
-/// Not sure if that's a good idea, but no clue how to fix.
-impl FromStr for $name {
-	type Err = anyhow::Error;
+init_compact_format!(SAR, [(start, f64), (increment, f64), (max, f64), (timeframe, Timeframe)]);
 
-	fn from_str(s: &str) -> Result<Self> {
-		let parts = s.split(PROTOCOL_ARGS_DELIMITER).collect::<Vec<_>>();
-		let mut fields = Vec::new();
-		$(
-		fields.push(stringify!($field));
-		)*
-		assert_eq!(parts.len(), fields.len(), "Incorrect number of parameters provided");
+init_compact_format!(TrailingStop, [(percent, f64)]);
 
-		let mut provided_params: HashMap<char, &str> = HashMap::new();
-		for param in s.split(PROTOCOL_ARGS_DELIMITER) {
-			if let Some(first_char) = param.chars().next() {
-				let value = &param[1..];
-				provided_params.insert(first_char, value);
-			}
-		}
-
-		Ok($name {
-		$(
-		$field: {
-			let first_char = stringify!($field).chars().next().unwrap();
-			let value = provided_params.get(&first_char).unwrap().parse::<$field_type>()?;
-			value
-		},
-		)*
-		})
-	}
-}
-
-impl fmt::Display for $name {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		let struct_name = stringify!($name).to_lowercase();
-		write!(f, "{}", struct_name)?;
-
-		$(
-			write!(f, "-{}{}", stringify!($field).chars().next().unwrap(), self.$field)?;
-		)*
-
-		Ok(())
-	}
-}
-};}
-//,}}}
-
-my_format!(SAR, [(start, f64), (increment, f64), (max, f64), (timeframe, Timeframe)]);
-
-my_format!(TrailingStop, [(percent, f64)]);
-
-my_format!(TpSl, [(tp, f64), (sl, f64)]);
+init_compact_format!(TpSl, [(tp, f64), (sl, f64)]);
 
 //TODO!!!: Slap a protocol slot on Position
 impl TrailingStop {
