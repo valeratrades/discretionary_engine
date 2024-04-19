52ef72e7a36c14c7b766fb09d9e529ac72b4e9c7
Valera6 <v79166789533@gmail.com>
2024-04-19 22:24:57 +0000

    checkpoint

--- src/utils.rs
+++ src/utils.rs
@@ -10,3 +10,22 @@ pub fn init_subscriber() {
 	let subscriber = Registry::default().with(env_filter).with(JsonStorageLayer).with(formatting_layer);
 	set_global_default(subscriber).expect("Failed to set subscriber");
 }
+
+//pub fn init_subscriber_orion() {
+//	let stdout_log = tracing_subscriber::fmt::layer().pretty();
+//	let file = File::create("debug.log");
+//	let file = match file {
+//		Ok(file) => file,
+//		Err(error) => panic!("Error: {:?}", error),
+//	};
+//	let debug_log = tracing_subscriber::fmt::layer().with_writer(std::sync::Arc::new(file));
+//
+//	tracing_subscriber::registry()
+//		.with(stdout_log
+//                // Add an `INFO` filter to the stdout logging layer
+//                .with_filter(filter::LevelFilter::TRACE)
+//                // Combine the filtered `stdout_log` layer with the
+//                // `debug_log` layer, producing a new `Layered` layer.
+//                .and_then(debug_log))
+//		.init();
+//}
