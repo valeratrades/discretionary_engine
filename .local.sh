9a80e28577bf25a1042239ff55d4044740ff29c3
valeratrades <v79166789533@gmail.com>
2024-07-14 17:56:21 +0000

    _

--- .local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# File is source on `cs` into the project's root. Allows to define a set of project-specific commands and aliases.
-
-#alias r="sc lrun -- start"
-
-alias t="c lrun -- new --tf=5m --size=-0.1 --coin=ADA"
-alias tb="c r -- --noconfirm new --tf=5m --size=0.1 --coin=ADA -f='ts:p-0.0005'"
-alias ts="c r -- --noconfirm new --tf=5m --size=-0.1 --coin=ADA -f='sar:t5m:s0.07:i0.02:m0.15'"
