85f49dac310186545cee0fff0148c414119bde45
Valera <valeratrades@gmail.com>
2025-11-24 16:00:37 +0000

    wip: fix errors for new command

--- discretionary_engine/src/adjust_pos.rs
+++ discretionary_engine/src/adjust_pos.rs
@@ -1,3 +1,4 @@
+use v_exchanges::Ticker;
 use v_utils::trades::Timeframe;
 
 #[derive(clap::Args, Debug)]
@@ -5,11 +6,16 @@ pub(crate) struct AdjustPosArgs {
 	/// Target change in exposure. So positive for buying, negative for selling.
 	#[arg(short, long)]
 	size_usdt: f64,
-	/// _only_ the coin name itself. e.g. "BTC" or "ETH". Providing full symbol currently will error on the stage of making price requests for the coin.
+
 	#[arg(short, long)]
-	coin: String,
+	ticker: Ticker,
+
 	/// timeframe, in the format of "1m", "1h", "3M", etc.
 	/// determines the target period for which we expect the edge to persist.
 	#[arg(short, long)]
 	tf: Option<Timeframe>,
+}
+
+pub(crate) fn main(args: AdjustPosArgs) -> Result<()> {
+	todo!();
 }
