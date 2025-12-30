38bb7c9b8edd7d9740b2140641b40de801b4b5f2
Valera <valeratrades@gmail.com>
2025-12-30 14:48:14 +0000

    chore: build.rs for versioning

--- discretionary_engine/src/main.rs
+++ discretionary_engine/src/main.rs
@@ -38,7 +38,7 @@ pub static MAX_CONNECTION_FAILURES: u32 = 10;
 pub static MUT_CURRENT_CONNECTION_FAILURES: AtomicU32 = AtomicU32::new(0);
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version = concat!(env!("CARGO_PKG_VERSION"), " (", env!("GIT_HASH"), ")"), about, long_about = None)]
 struct Cli {
 	#[command(subcommand)]
 	command: Commands,
